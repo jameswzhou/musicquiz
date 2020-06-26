850169af078abb5d2019b40849d7ce8599252999
st0ro <46041969+st0ro@users.noreply.github.com>
2020-06-26 01:00:20 -0600

    Deezer API integration
    Now pulls playlists from Deezer API. Multiple playlists supported with protection against identical song entries (although not against similar songs released differently).

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,48 @@
 const http = require('http');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain',
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
-});
-  res.end('https://cdns-preview-8.dzcdn.net/stream/c-840ed62a497746762716dd503386d476-5.mp3');
+	res.writeHead(200, {
+		'Content-Type': 'text/plain',
+		'Access-Control-Allow-Origin' : '*',
+		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+	});
+	res.end('https://cdns-preview-8.dzcdn.net/stream/c-840ed62a497746762716dd503386d476-5.mp3');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/\n`);
+});
+
+var lineReader = require('readline').createInterface({
+	input: require('fs').createReadStream('playlists.txt')
+});
+
+var tracklist = [];
+
+lineReader.on('line', function (line) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var apiResult = JSON.parse(this.responseText);
+			console.log("\nLoaded playlist " + apiResult.title + " with " + apiResult.nb_tracks + " songs.");
+			for(let i = 0; i < apiResult.tracks.data.length; i++){
+				let duplicate = false;
+				for(let j = 0; j < tracklist.length; j++){
+					if(tracklist[j].id == apiResult.tracks.data[i].id){
+						duplicate = true;
+					}
+				}
+				if(!duplicate){
+					tracklist.push(apiResult.tracks.data[i]);
+					console.log("Added " + apiResult.tracks.data[i].title + " to the song pool.");
+				}
+			}
+		}
+	};
+	xhttp.open("GET", "https://api.deezer.com/playlist/" + line, true);
+	xhttp.send();
 });
