927060df6ae4c646b00d3cee9517677f53a9791a
rosieyzh <rosieyzh@gmail.com>
2020-06-30 19:42:39 -0400

    updated js and css for merge conflicts

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,23 @@
+var audioElement;
+function add(){
+  var list = document.getElementById("players");
+  list.innerHTML += "<li>Rachel</li>";
+}
 function test(){
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var audioElement = new Audio(this.responseText);
-	  audioElement.volume = 0.1;
-	  audioElement.play();
-    }
-  };
-  xhttp.open("GET", "http://localhost:3000/", true);
-  xhttp.send();
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+  audioElement = new Audio(this.responseText);
+  audioElement.volume = 0.1;
+  audioElement.play();
+  }
+};
+xhttp.open("GET", "http://localhost:3000/", true);
+xhttp.send();
 }
 
-function add(){
-      var list = document.getElementById("players");
-      list.innerHTML += "<li>Rachel</li>";
+function stop(){
+  if(!audioElement.paused){
+    audioElement.pause();
+  }
 }
