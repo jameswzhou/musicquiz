43a82aae98ae849467e4ae471df7c40c7a28c3d3
st0ro <46041969+st0ro@users.noreply.github.com>
2020-06-30 18:01:44 -0600

    Server basic functionality implemented
    Rooms are able to start and progress through songs every 20 seconds.

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,50 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+var scoreboard = { roomStarted: false, currentSong: 0, currentSongFile: "", playerList: [] };
+var nameList = new Array('iAmSpeed', 'uwu', 'big pp', 'plussyfan9', 'b@tman', 'javascript time', 'we live in a society', '◔ ⌣ ◔', 'herro', 'Barack Obama');
+var timer;
+
 app.use(cors());
 
-app.get('/', (req, res) => res.send('https://cdns-preview-8.dzcdn.net/stream/c-840ed62a497746762716dd503386d476-5.mp3'));
+app.get('/scoreboard/', function (req, res) {
+	res.send(JSON.stringify(scoreboard));
+});
 
-app.listen(port, function() {
+app.post('/join/', function (req, res) {
+	let newPlayer = new Player(nameList[scoreboard.playerList.length]);
+	scoreboard.playerList.push(newPlayer);
+	res.send(newPlayer.name);
+	console.log(`${newPlayer.name} joined.`);
+});
+
+app.post('/start/', function (req, res) {
+	if (!scoreboard.roomStarted) {
+		scoreboard.roomStarted = true;
+		console.log('Starting room.');
+		scoreboard.currentSongFile = tracklist[scoreboard.currentSong].preview;
+		for (let i = 1; i < scoreboard.playerList.length; i++) {
+			scoreboard.playerList[i].solvedName = false;
+			scoreboard.playerList[i].solvedArtist = false;
+		}
+		timer = setInterval(function () {
+			scoreboard.currentSong++;
+			scoreboard.currentSongFile = tracklist[scoreboard.currentSong].preview;
+			for (let i = 1; i < scoreboard.playerList.length; i++) {
+				scoreboard.playerList[i].solvedName = false;
+				scoreboard.playerList[i].solvedArtist = false;
+			}
+		}, 20000);
+	}
+});
+
+app.listen(port, function () {
 	console.log(`Server listening at http://localhost:${port}`);
 });
+
+function Player(name) {
+	this.name = name;
+	this.score = 0;
+	this.solvedName = false;
+	this.solvedArtist = false;
+}
