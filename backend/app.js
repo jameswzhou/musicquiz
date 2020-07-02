79a400f455ae16875595575a5e01553b5b80896d
st0ro <46041969+st0ro@users.noreply.github.com>
2020-07-02 11:07:47 -0600

    Updates to track loading
    Each track is loaded individually, but needs to be updated to be asynchronous as startup is very slow

--- backend/app.js
+++ backend/app.js
@@ -28,9 +28,9 @@ app.get('/scoreboard/', function (req, res) {
 });
 
 app.post('/join/', function (req, res) {
-	let newPlayer = new Player(nameList[scoreboard.playerList.length]);
+	let newPlayer = new Player(nameList[scoreboard.playerList.length], scoreboard.playerList.length);
 	scoreboard.playerList.push(newPlayer);
-	res.send(newPlayer.name);
+	res.send(JSON.stringify(newPlayer));
 	console.log(`${newPlayer.name} joined.`);
 });
 
@@ -38,11 +38,7 @@ app.post('/start/', function (req, res) {
 	if (!scoreboard.roomStarted) {
 		scoreboard.roomStarted = true;
 		console.log('Starting room.');
-		scoreboard.currentSongFile = tracklist[scoreboard.currentSong].preview;
-		for (let i = 1; i < scoreboard.playerList.length; i++) {
-			scoreboard.playerList[i].solvedName = false;
-			scoreboard.playerList[i].solvedArtist = false;
-		}
+		scoreboard.currentSongFile = tracklist[0].preview;
 		timer = setInterval(function () {
 			scoreboard.currentSong++;
 			scoreboard.currentSongFile = tracklist[scoreboard.currentSong].preview;
@@ -54,13 +50,54 @@ app.post('/start/', function (req, res) {
 	}
 });
 
+app.put('/guess/', function (req, res) {
+	let id = req.param('id');
+	let guess = req.param('data');
+	if (!scoreboard.playerList[id].solvedName && checkSongName(guess)) {
+		scoreboard.playerList[id].solvedName = true;
+		res.send('correct');
+		increaseScore(id);
+		return;
+	}
+	if (!scoreboard.playerList[id].solvedArtist && checkArtistName(guess)) {
+		scoreboard.playerList[id].solvedArtist = true;
+		res.send('correct');
+		increaseScore(id);
+		return;
+	}
+	res.send('incorrect');
+});
+
 app.listen(port, function () {
 	console.log(`Server listening at http://localhost:${port}`);
 });
 
-function Player(name) {
+function Player(name, id) {
 	this.name = name;
 	this.score = 0;
+	this.playerId = id;
 	this.solvedName = false;
 	this.solvedArtist = false;
+}
+
+function checkSongName(guess) {
+	return tracklist[scoreboard.currentSong].parsedTitle === guess;
+}
+
+function checkArtistName(guess) {
+	let song = tracklist[scoreboard.currentSong];
+	for(let i = 0; i < song.contributors.length; i++){
+		if(song.contributors[i] === guess){
+			return true;
+		}
+	}
+	return false;
+}
+
+function increaseScore(playerId){
+	var player = scoreboard.playerList[playerId];
+	player.score += 2;
+	if(player.solvedName && player.solvedArtist){
+		player.score += 3;
+	}
 }
--- backend/playlistTools.js
+++ backend/playlistTools.js
@@ -4,31 +4,34 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 module.exports = {
     loadPlaylist(playlistId) {
         var xhttp = new XMLHttpRequest();
-        var result;
+        var result = [];
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 var apiResult = JSON.parse(this.responseText);
                 console.log(`Loaded playlist ${apiResult.title} with ${apiResult.nb_tracks} songs.`);
-                result = apiResult.tracks.data;
+                var tracks = apiResult.tracks.data;
+                for (let i = 0; i < tracks.length; i++) {
+                    result.push(loadTrack(tracks[i].id));
+                }
             }
         };
         xhttp.open("GET", "https://api.deezer.com/playlist/" + playlistId, false);
         xhttp.send();
         return result;
     },
-    shuffleTracks(tracklist, consoleCheats){
-        for(let i = tracklist.length - 1; i > 0; i--){
+    shuffleTracks(tracklist, consoleCheats) {
+        for (let i = tracklist.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [tracklist[i], tracklist[j]] = [tracklist[j], tracklist[i]];
         }
         console.log(`Shuffled ${tracklist.length} tracks.`);
-        if(consoleCheats){
-            for(let i = 0; i < tracklist.length; i++){
+        if (consoleCheats) {
+            for (let i = 0; i < tracklist.length; i++) {
                 console.log(`>${tracklist[i].title}`);
             }
         }
     },
-    mergeTracks(tracklist, list){
+    mergeTracks(tracklist, list) {
         for (let i = 0; i < list.length; i++) {
             let duplicate = false;
             for (let j = 0; j < tracklist.length; j++) {
@@ -38,8 +41,48 @@ module.exports = {
             }
             if (!duplicate) {
                 tracklist.push(list[i]);
-                console.log(`Added ${list[i].title} to the song pool.`);
             }
         }
     }
 };
+
+function loadTrack(trackId) {
+    var trackXhr = new XMLHttpRequest();
+    var result;
+    trackXhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var trackResult = JSON.parse(this.responseText);
+            console.log(`Loaded track ${trackResult.title} with ${trackResult.contributors.length} contributor(s).`);
+            result = new Track(trackResult);
+        }
+    };
+    trackXhr.open("GET", "https://api.deezer.com/track/" + trackId, false);
+    trackXhr.send();
+    return result;
+}
+
+function Track(track) {
+    this.id = track.id;
+    this.title = track.title;
+    this.parseSongTitle = parseSongName(this.name);
+    this.preview = track.preview;
+    this.image = track.album.cover;
+    this.link = track.link;
+    this.contributors = [];
+    this.parsedContributors = [];
+    for(let i = 0; i < track.contributors.length; i++){
+        this.addContributor(track.contributors[i].name);
+    }
+}
+Track.prototype.addContributor = function (contributorName) {
+    this.contributors.push(contributorName);
+    this.parsedContributors.push(parseArtistName(contributorName));
+}
+
+function parseSongName(name) {
+    return name;
+}
+
+function parseArtistName(name) {
+    return name;
+}
