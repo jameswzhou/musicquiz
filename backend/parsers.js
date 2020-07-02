9cc7614d9931d2d63e4ec589c51546d7134599b3
jameswzhou <56765078+jameswzhou@users.noreply.github.com>
2020-07-02 16:55:25 -0600

    Update parsers.js
    Actually fixed the functions

--- backend/parsers.js
+++ backend/parsers.js
@@ -2,13 +2,18 @@ function checkSongName(title) {
     var newtitle = title.trim();
     newtitle = newtitle.toLowerCase();
     newtitle = newtitle.replace(/\([^()]*\)/g, '');
+    newtitle = newtitle.replace(/[^a-zA-Z0-9]/g, "");
+    newtitle = newtitle.replace(" ", "");
     return newtitle;
 }
 
-function checkAristName(name) {
+
+
+function checkArtistName(name) {
     var newname = name.trim();
     newname = newname.toLowerCase();
     newname = newname.replace("é", "e");
     newname = newname.replace(/[^a-zA-Z0-9]/g, "");
+    newname = newname.replace(" ", "");
     return newname;
 }
